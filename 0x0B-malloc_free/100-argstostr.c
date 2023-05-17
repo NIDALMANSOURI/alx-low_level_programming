aa9f5d8ddfe26e1e6e3ac4fb77665b115c581036
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-05-17 01:33:25 -0700

    Task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -27,8 +27,8 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (i = 0; i < ac; i++; nc++)
-		nc += -strlen(av[i]);
+	for (i = 0; i < ac; i++, nc++)
+		nc += _strlen(av[i]);
 
 	s = malloc(sizeof(char) * nc + 1);
 	if (s == 0)
@@ -36,7 +36,7 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++; cmpt++)
+		for (j = 0; av[i][j] != '\0', j++; cmpt++)
 			s[cmpt] = av[i][j];
 
 		s[cmpt] = '\n';
