fe86868f2cf272b1dfb4c1023bbe5aa65d801ab6
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-05-19 02:56:35 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,6 +12,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 	while (n--)
 		*s++ = b;
+
 	return (ptr);
 }
 
@@ -27,11 +28,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	m = malloc(sizeof(int) * nmemb);
+	m = malloc(size * nmemb);
 
 	if (m == 0)
 		return (NULL);
-	_memset(m, 0, sizeof(int) * nmemb);
+
+	_memset(m, 0, size * nmemb);
 
 	return (m);
 }
