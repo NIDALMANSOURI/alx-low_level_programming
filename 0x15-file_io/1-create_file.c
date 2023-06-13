545c655d87559436dcbf2182540672e15815fb08
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-06-13 08:59:35 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,6 +34,6 @@ int create_file(const char *filename, char *text_content)
 	if (len)
 		bytes = write(f, text_content, len);
 	close(f);
-	return (bytes = len ? 1 : -1);
+	return (bytes == len ? 1 : -1);
 }
 
