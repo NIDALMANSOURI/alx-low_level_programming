d08d10c8c682a6e8e03610c02cad2eefb2557738
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-07-06 15:48:02 -0700

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int b = sizeof(n) * 8;
+	int p = 0;
+
+	while (b)
+	{
+		if (n & 1L << --b)
+		{
+			_putchar('1');
+			p++;
+		}
+		else if (p)
+			_putchar('0');
+	}
+	if (!p)
+		_putchar('0');
+}
