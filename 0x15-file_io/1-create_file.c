519b58543c418b7a2cb288057432674406747009
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-07-10 17:45:41 -0700

    Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * _strlen - return the length of string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+	f = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (f == -1)
+		return (-1);
+	if (len)
+		bytes = write(f, text_content, len);
+	close(f);
+	return (bytes == len ? 1 : -1);
+}
+
