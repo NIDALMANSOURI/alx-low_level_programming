5e8b3b1376382d540c98b732399870ddc0faa4ab
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-07-10 18:20:31 -0700

    Task 5

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -14,4 +14,26 @@ void print_magic(Elf64_Ehdr h)
 	for (c = 0; c < EI_NIDENT; c++)
 		printf("%2.2x%s", h.e_ident[c], c == EI_NIDENT - 1 ? "\n" : " ");
 }
+/**
+ * print_class - print ELF class
+ * @h: ELF header struct
+ */
+void print_class(Elf64_Ehdr h)
+{
+	printf("  Class:                             ");
+	switch (h.e_ident[EI_CLASS])
+	{
+		case ELFCLASS64:
+			printf("ELF64");
+			break;
+		case ELFCLASS32:
+			printf("ELF32");
+			break;
+		case ELFCLASSNONE:
+			printf("none");
+			break;
+	}
+	printf("\n");
+}
+
 
